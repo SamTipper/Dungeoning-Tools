c1f4c181dc5bd613ca4990f699f4db2fa6d18246
SamTipper <samtipperprogrammer@gmail.com>
2023-02-07 21:24:32 +0000

    API updates

--- API/index.js
+++ API/index.js
@@ -1,15 +1,39 @@
+const firebase = require('firebase');
 const express = require('express');
+const constants = require('./constants');
+
 const app = express();
-
-
+firebase.initializeApp(constants.firebaseConfig);
+const db = firebase.firestore();
 const port = 15212;
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
 
-app.get('', (req, res) => {
-    res.send("Sup dood");
-})
+app.get('/set_campaign', async (req, res) => {
+    try {
+        db.collection('campaigns').doc(constants.docId).update({
+            "Arcane King Campaign": {
+                players: []
+            }
+        });
+        return res.status(200).send("Success"); 
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
 
-app.get()
+app.get('/get_campaign', async (req, res) => {
+    try {
+        db.collection('campaigns').get().then(
+            querysnapshot => {
+            for (let doc of querysnapshot.docs){
+                console.log(doc.data());
+            }
+            return res.status(200).send("Success");
+        });
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});

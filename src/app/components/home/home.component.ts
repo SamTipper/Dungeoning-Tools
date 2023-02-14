0ffd5b700cdc20cd2d849466dee33c079e5a222f
SamTipper <rogoclock@gmail.com>
2023-02-14 18:42:29 +0100

    form changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class HomeComponent implements OnInit{
   existingCampaignForm: FormGroup;
+
+  constructor(
+    private router: Router
+  ) { }
   
   ngOnInit(){
     this.existingCampaignForm = new FormGroup({
       'campCode': new FormControl(null, [Validators.required])
     })
+  }
+
+  onSubmit(){
+    if (this.existingCampaignForm.value.campCode){
+      // User enters a campaign code
+    } else {
+      // User wants to create a campaign
+      this.router.navigate(['create-campaign']);
+    }
   }
 }

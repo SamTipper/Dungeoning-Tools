55d5b560b548d6dedf166ee4954a4a24e5feff43
SamTipper <rogoclock@gmail.com>
2023-02-14 20:07:22 +0100

    Able to add campaign names and players

--- src/app/components/create-campaign/create-campaign.component.ts
+++ src/app/components/create-campaign/create-campaign.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-campaign',
   templateUrl: './create-campaign.component.html',
   styleUrls: ['./create-campaign.component.css']
 })
-export class CreateCampaignComponent {
+export class CreateCampaignComponent implements OnInit{
+  newCampaignForm: FormGroup;
+  players: string[] = [null];
+
+  ngOnInit(){
+    this.newCampaignForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required])
+    })
+  }
+
+  onSubmit(){
+
+  }
+
+  onAddPlayer(){
+    this.players.push(null);
+  }
+
+  onPlayerChange(event, index){
+    this.players[index] = event;
+    console.log(JSON.stringify(this.players));
+  }
+
+  trackByFn(index, treatment) {
+    return index;
+  }
 
 }

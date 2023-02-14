45cfb42652ac7804c65c871d8849b922f3990937
SamTipper <rogoclock@gmail.com>
2023-02-15 00:24:06 +0100

    Quick roll on nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dungeoning-tools';
+  rolling: boolean;
+  quickDice: number = 20;
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async rollDice(){
+    this.rolling = true;
+    for (let i = 0; i < Math.floor(Math.random() * (20 - 3) + 3); i++){
+      this.quickDice = Math.floor(Math.random() * (21 - 1) + 1);
+      await this.sleep(80);
+    }
+    this.rolling = false;
+  }
 }

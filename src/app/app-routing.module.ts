cda58d05855ffa213930d6d31fbeda0d29ab7445
SamTipper <samtipperprogrammer@gmail.com>
2023-02-20 19:11:59 +0000

    Added dice roller component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCampaignComponent } from './components/create-campaign/create-campaign.component';
+import { DiceRollerComponent } from './components/dice-roller/dice-roller.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'create-campaign', component: CreateCampaignComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'create-campaign', component: CreateCampaignComponent },
+  { path: 'dice-roller', component: DiceRollerComponent}
 ];
 
 @NgModule({
---
+++ src/app/components/dice-roller/dice-roller.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DiceRollerComponent } from './dice-roller.component';
+
+
+@NgModule({
+  declarations: [
+    DiceRollerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DiceRollerModule { }
